38f948156203854767892ab2561c784889adbbd9
eiji03aero <eiji03aero@gmail.com>
2017-08-15 11:38:16 +0900

    modal and css

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 //variables
 var express = require('express'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    MongoClient = require('mongodb').MongoClient,
-    assert = require('assert'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  assert = require('assert'),
 
-    //controller
-    todoLists = require('./routes/todoLists'),
+  //controller
+  todoLists = require('./routes/todoLists'),
 
-    app = express();
-
+  app = express();
 
 //view settings
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-
 //middlewares
 app.use(express.static(__dirname + '/views/assets'));
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-
 
 //routings
 app.get('/', todoLists.index);
@@ -31,13 +27,11 @@ app.post('/todoLists/create', todoLists.create);
 app.put('/todoLists/:id', todoLists.update);
 app.delete('/todoLists/:id', todoLists.destroy);
 
-
 //error handling
 app.use(function(err, req, res, next) {
   console.log(err.stack);
   res.status(500).send('something went wrong...');
-})
-
+});
 
 //server
 app.listen(3000);
