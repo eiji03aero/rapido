42f5b4f0b71ed188b1b7db9bd424a7b4437cf9cf
eiji03aero <eiji03aero@gmail.com>
2017-08-21 11:35:38 +0900

    made progress on modal design

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,10 +2,17 @@ const modalTime = 200;
 
 $(function() {
   var timer = null;
-  $('.modals').hide();
-  $(".open-edit-modal").click(function() {
+  $('.js-modal').hide();
+
+  $(".js-modal--create__open").click(function() {
     modalResize();
-    $("#modal-bg, #modal-field, #edit-todo").fadeIn(modalTime);
+    $(".js-modal__background, .js-modal__field, .js-modal--create").fadeIn(modalTime);
+    $('.js-modal--content:not(.js-modal--create)').hide();
+  });
+
+  $(".js-modal--edit__open").click(function() {
+    modalResize();
+    $(".js-modal__background, #modal-field, #edit-todo").fadeIn(modalTime);
     $('.modal-content:not(#edit-todo)').hide();
     let todo = $(this).data('todo'),
         action = $('#editForm').attr('action') + todo._id + '?_method=PUT';
@@ -14,25 +21,18 @@ $(function() {
     $('.description-body p').html(todo.contents[0].description);
     $('#editID').attr('value', todo._id);
     $('#createdAt p').html("Created at: "+todo.contents[0].createdAt);
-    console.log(todo);
   });
 
-  $("#open-create-modal").click(function() {
-    modalResize();
-    $("#modal-bg, #modal-field, #create-todo").fadeIn(modalTime);
-    $('.modal-content:not(#create-todo)').hide();
-  });
-
-  $("#modal-bg").click(function() {
-    $(".modals").fadeOut(modalTime);
+  $(".js-modal__background").click(function() {
+    $(".js-modal").fadeOut(modalTime);
   });
 
   function modalResize() {
     let h = $(window).height(),
         w = $(window).width(),
-        mh = $("#modal-field").outerHeight(),
-        mw = $("#modal-field").outerWidth();
-    $("#modal-field").css({
+        mh = $(".js-modal__field").outerHeight(),
+        mw = $(".js-modal__field").outerWidth();
+    $(".js-modal__field").css({
       "top": ((h-mh)/2) + "px",
       "left": ((w-mw)/2) + "px",
     });
